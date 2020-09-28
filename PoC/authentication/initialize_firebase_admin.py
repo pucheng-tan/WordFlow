d82b773616713630aa0e9d7f75f4b692c8e516b9
taramepp <taramepp@gmail.com>
2020-09-28 16:27:43 -0600

    Working on some basic authentication with Firebase. Got create user, get user, delete user going (update user will be easy). Also authenticated the user using the requests module. I broke it because I don't understand truthiness in python, see "TODO" to find the error. -Tara

---
+++ PoC/authentication/initialize_firebase_admin.py
@@ -0,0 +1,9 @@
+import firebase_admin
+from firebase_admin import credentials
+
+cred = firebase_admin.credentials.Certificate('./python-admin/cmpt370-group2-firebase-adminsdk-lno8j-b5a0b9573b.json')
+app = firebase_admin.initialize_app(cred)
+# app = firebase_admin.initialize_app()
+
+if __name__ == "__main__":
+    print(app)
