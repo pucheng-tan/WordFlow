9d8b780d77ecb0ac68294c9849f1fd10280d8b4e
DESKTOP-QAQ5QQV\Sarah <sarahc19233@gmail.com>
2020-11-08 23:05:11 -0600

    Added create new school and separated creating and placing labels

--- login.py
+++ login.py
@@ -9,19 +9,24 @@ class Login(tk.Frame):
         self.master.title("Welcome")
 
         self.create_labels()
+        self.create_clickable_labels()
         self.create_entries()
         self.create_buttons()
 
     def create_labels(self):
-        # Permanent labels
-        self.school_label = tk.Label(self.master, text="School").grid(row=0)
-        self.email_label = tk.Label(self.master, text="Email").grid(row=2)
-        self.password_label = tk.Label(self.master, text="Password").grid(row=4)
+        # Create permanent labels
+        self.school_label = tk.Label(self.master, text="School")
+        self.email_label = tk.Label(self.master, text="Email")
+        self.password_label = tk.Label(self.master, text="Password")
         self.new_user_label = tk.Label(self.master, text="New User")
 
-        # Temporary labels
+        # Create temporary labels
         self.forget_label = tk.Label(self.master, text="You are missing a field!", fg="red")
 
+        # Place Permanent labels into their positions
+        self.school_label.grid(row=0)
+        self.email_label.grid(row=2)
+        self.password_label.grid(row=4)
         self.new_user_label.grid(row=8)
 
     def create_entries(self):
@@ -30,9 +35,20 @@ class Login(tk.Frame):
         self.email_entry = tk.Entry(self.master)
         self.password_entry = tk.Entry(self.master)
 
-        self.school_entry.grid(row=1, padx=10, pady=10)
+        self.school_entry.grid(row=1, padx=10, pady=5)
         self.email_entry.grid(row=3)
         self.password_entry.grid(row=5)
+
+    def create_clickable_labels(self):
+
+        self.create_school_label = tk.Label(self.master, text="Create School", fg="blue")
+        self.create_school_label.configure(font="Verdana 9 underline")
+
+        self.create_school_label.grid(row=10)
+        self.create_school_label.bind("<Button>", self.mouse_click)
+
+    def mouse_click(self, event):
+        print("I was clicked")
 
     def create_buttons(self):
 
@@ -43,7 +59,7 @@ class Login(tk.Frame):
 
         sign_up = tk.Button(self.master, text="Sign Up", fg="white", bg="blue")
         sign_up["command"] = self.sign_up
-        sign_up.grid(row=9, padx=10, pady=10)
+        sign_up.grid(row=9, padx=10, pady=5)
 
     def sign_in(self):
         school = self.school_entry.get()
