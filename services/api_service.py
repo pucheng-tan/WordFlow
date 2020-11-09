dd704eb1a42f0a605ad8689c7d88536d1773b793
Abdul <abdulbasitsalawu@yahoo.ca>
2020-11-09 17:31:00 -0600

    Wrote basic methods for get in APIService. Did some debugging but tests are still not passing, its not reading the query correctly. Peer Programming with Tara

--- services/api_service.py
+++ services/api_service.py
@@ -3,15 +3,94 @@ from firebase_admin import credentials
 from firebase_admin import firestore
 
 class APIService:
+    """Direct interface with the API
+    Usage:
+        get: use to retrieve data from the database
+        post: use to save data from the database
+    """
 
     WHERE_CLAUSE = "where"
     SEARCH_ALL = "all"
+    EXISTS = "exists"
+    _db = None
+    _projectID = "cmpt370-group2"
+    
+    # Web API key = "AIzaSyDg_L6PD2cbSSFUTEpET56VOVKJaGWm5Nc"
 
     def __init__(self):
-        pass
+        # this class needs to be singleton, following tutorial from here: https://www.tutorialspoint.com/python_design_patterns/python_design_patterns_singleton.htm
+        # if (APIService._db is not None):
+        #     raise Exception("This class is a singleton!")
+        # else:
+        #     cred = credentials.ApplicationDefault()
+        #     firebase_admin.initialize_app(cred, {
+        #     'projectId': APIService._projectID,
+        #     })
+        # APIService._db = firestore.client()
 
-    def get(self, path, where_clause=None):
-        return 0
+        if (APIService._db is not None):
+            raise Exception("This class is a singleton!")
+        else:
+            # TODO: This is not how credentials should be setup.
+            # If this is ever called more than once, the initialize_app gives an error because it is only ever meant to be called once!
+            # The JSON is credentials for a service account that's actually not supposed to go into a public repository.
+            cred = credentials.Certificate(
+                'PoC/services/cmpt370-group2-firebase-adminsdk-lno8j-3910eb45cf.json'
+            )
+            firebase_admin.initialize_app(cred)
+            APIService._db = firestore.client()
+
+
+    def get(self, path, where_clauses=None):
+        query = self._get_db()
+        # path = {"Schools": TEST_SCHOOL_ID, "UserProfiles": TEST_USER_ID, "History": APIService.WHERE_CLAUSE}
+        # path_2 = ({"History": APIService.SEARCH_ALL}, [["UserProfile", "==", "BMSoKUzVnRYn103t2Oi6Nacv9X03"], ["AssignmentID", "exists"]], "2020101513091234"),
+        keys = list(path.keys())
+        for collection in path:
+            if path[collection] not in [APIService.WHERE_CLAUSE, APIService.SEARCH_ALL]:
+                query = query.collection(collection).document(path[collection])
+            else:
+                # Todo: Figure out decision tree 
+                pass
+
+        if path[keys[-1]] == APIService.SEARCH_ALL:
+            query = query.collection_group(keys[-1])
+
+        if path[keys[-1]] == APIService.WHERE_CLAUSE:
+            query = query.collection(keys[-1])
+            query = self._get_where_clauses(query, where_clauses)
+        
+        return self._to_dict(query, where_clauses == None)
+
 
     def post(self, params):
         return 0
+
+
+
+    @staticmethod
+    def _get_db():
+        if APIService._db == None:
+            APIService()
+        return APIService._db
+
+    
+    def _get_where_clauses(self, query, where_clauses):
+        # Loop through and append each to the query
+        q = query
+        for clause in where_clauses:
+            if APIService.EXISTS in clause:
+                q = q.order_by(clause[0])
+            else:
+                q = q.where(clause[0], clause[1], clause[2])
+
+        return q
+
+    def _to_dict(self, query, is_document):
+        if (is_document):
+            return query.get().to_dict()
+        else:
+            data = query.stream()
+            data = {doc.id: doc.to_dict() for doc in data}
+            return data
+        
--- services/test_api_service.py
+++ services/test_api_service.py
@@ -10,34 +10,36 @@ api = APIService()
 TEST_SCHOOL_ID = "o2lTSAI6X4yGdIZ0huB9"
 TEST_CLASSROOM_ID = "uWkea95OBZ1d94kaPThK"
 TEST_USER_ID = "BMSoKUzVnRYn103t2Oi6Nacv9X03"
-TEST_DATE = datetime(2020, 11, 1, tzinfo=timezone.utc)# November 1
+TEST_DATE = datetime(2020, 10, 30, tzinfo=timezone.utc)# November 1
 
 
 
 # test valid get queries
 @pytest.mark.api_call
 @pytest.mark.parametrize("path, where_clause, expected_reference", [
-    ({"Schools": TEST_SCHOOL_ID, "UserProfiles": TEST_USER_ID, "History": APIService.WHERE_CLAUSE}, [["DateCompleted", ">", TEST_DATE], ["Mode", "==", "0"], ["Duration", "in", [120]]], "2020101513071234"),
+    ({"Schools": TEST_SCHOOL_ID, "UserProfiles": TEST_USER_ID, "History": APIService.WHERE_CLAUSE}, [["Mode", "==", "0"]], "2020101513071234"),
     ({"History": APIService.SEARCH_ALL}, [["UserProfile", "==", "BMSoKUzVnRYn103t2Oi6Nacv9X03"], ["AssignmentID", "exists"]], "2020101513091234"),
-    ({"Schools": TEST_SCHOOL_ID, "Classrooms": APIService.WHERE_CLAUSE}, [["Members", "array-contains", TEST_USER_ID]], "uWkea95OBZ1d94kaPThK"),
+    ({"Schools": TEST_SCHOOL_ID, "Classrooms": APIService.WHERE_CLAUSE}, [["Members", "array_contains", TEST_USER_ID]], "uWkea95OBZ1d94kaPThK"),
     ({"Schools": TEST_SCHOOL_ID, "Classrooms": TEST_CLASSROOM_ID}, [], TEST_CLASSROOM_ID),
 ])
-def test_get_valid(path, where_clause, json_metadata):
+def test_get_valid(path, where_clause, expected_reference, json_metadata):
     json_metadata['id'] = "UT API.1"
     json_metadata['description'] = "Testing various valid 'get' queries with the API"
-
+    
     # Parameters:
     # 1. path: Schools (School X), User Profiles (Pucheng), History. Where: [Date Completed > Nov 1st midnight, Mode == 0, UserProfile in [doc ref: "/Schools/x school/UserProfiles/pucheng"]]
+    # ["DateCompleted", ">", TEST_DATE],,   ["Duration", "in", [120]]
     # 2. path: Collection Group: History. Where: [UserProfile == "pucheng"]
     # 3. path: Schools (School X), Classrooms. Where: [Members array-contains pucheng]
     # 4. path: Schools (School X), Classroms (X Classroom). 
     
     # perform the query
+    pytest.set_trace()
     results = api.get(path, where_clause)
 
     # if there is a where clause, results will come back in a list
     if len(where_clause) > 0:
-        # 
+        results = list(results)
         assert len(results) == 1
         document_reference = results[0].reference
 
@@ -52,7 +54,7 @@ def test_get_valid(path, where_clause, json_metadata):
 @pytest.mark.parametrize("path, where_clause", [
     ({"Schools": TEST_SCHOOL_ID, "UserProfiles": TEST_USER_ID, "History": APIService.WHERE_CLAUSE}, ["Mode", "==", 0]),
     ({"History": APIService.SEARCH_ALL}, []),
-    ({0: TEST_SCHOOL_ID})
+    ({0: TEST_SCHOOL_ID}, [])
 ])
 def test_get_invalid(path, where_clause, json_metadata):
     json_metadata['id'] = "UT API.2"
