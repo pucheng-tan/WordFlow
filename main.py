a2efa7cb87b91aa001a2fed8b587803d0b71659f
DESKTOP-QAQ5QQV\Sarah <sarahc19233@gmail.com>
2020-11-09 23:06:26 -0600

    Added documentation to and run yapf over main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+"""Runs the authentication screen."""
+
 import tkinter as tk
 import login
 
+
 def main():
+    """Runs the authentication screen."""
+
     root = tk.Tk()
     app = login.Authentication(master=root)
     app.mainloop()
+
 
 if __name__ == "__main__":
     main()
