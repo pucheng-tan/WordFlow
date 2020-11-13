2a7edee4a29c10e9880d3c0c786d749eaaf5e786
Cameron Mcleod <cvm791@mail.usask.ca>
2020-11-13 13:47:47 -0600

    refactored GUI code, started typing challenge

--- userInterface/mainMenuWindow.py
+++ userInterface/mainMenuWindow.py
@@ -1,17 +1,19 @@
-from activeWindow import activeWindow
+from BaseWindow import baseWindow
 from tkinter import *
 
-class mainMenu(activeWindow):
+class mainMenu(baseWindow):
     """This is the module for the main menu window
     """
+
     def __init__(self, master, priviledge="user"):
         super().__init__(master,priviledge, True,windowName="Main Menu")
-        
+        super().createWindow()
         self.mainMenuMainFrame()
+        
     
 
 
     def mainMenuMainFrame(self):
-        welcomeLabel = Label(self.mainFrame,text="Welcome <Username Placeholder>")
+        welcomeLabel = Label(self.activeWindow,text="Welcome <Username Placeholder>")
         welcomeLabel.pack(pady=10)
 
--- userInterface/newChallengeWindow.py
+++ userInterface/newChallengeWindow.py
@@ -1,16 +1,42 @@
+from BaseWindow import baseWindow
 from activeWindow import activeWindow
 from tkinter import *
 
-class newChallengeMenu(activeWindow):
+
+class newChallengeMenu(baseWindow):
     """This is the module for the main menu window
     """
     def __init__(self, master, priviledge="user"):
         super().__init__(master, priviledge, False, windowName="New Challenge")
-        
+        super().createWindow()
         self.newChallengeMainFrame()
     
 
 
     def newChallengeMainFrame(self):
-        welcomeLabel = Label(self.mainFrame,text="This is a placeholder for the new challenge screen!")
+        welcomeLabel = Label(self.activeWindow,text="This is a placeholder for the new challenge screen!")
         welcomeLabel.pack(pady=10)
+        #This variable will be set the the type of test the user wants
+        challengeType = StringVar(self.master)
+        challengeType.set("Standard") 
+        #Create the option menu
+        chooseChallengeTypeDropdown = OptionMenu(self.activeWindow, challengeType, "Standard", "Programming Test (Under development)", "Dictation Test (Under development)")
+        chooseChallengeTypeDropdown.pack()
+
+        submitButton = Button(self.activeWindow, text="Start",command=self._loadTypingChallenge)
+        submitButton.pack()
+
+    def _loadTypingChallenge(self):
+        
+        self.activeWindow.destroy()
+        self.activeWindow = None
+
+        self.activeWindow = activeWindow(self.mainFrame).getItems()
+        self.activeWindow.pack(side=RIGHT)
+        self.activeWindow.pack_propagate(0)
+
+
+
+        
+
+
