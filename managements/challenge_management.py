e511f4f93eebd54a6cd5b0955bbd0fabd8775019
taramepp <taramepp@gmail.com>
2020-11-21 14:42:50 -0600

    Looks like I had accidentally made ChallengeService and ChallengeManagement the same text context due to a copy and paste error last time, so now they are properly separated. Not really any changes yet, but I want the updates on master.

--- managements/challenge_management.py
+++ managements/challenge_management.py
@@ -6,10 +6,22 @@ class ChallengeManagement(ApplicationManagement):
 
     _service = ChallengeService()
 
-    def get_random_challenge_content(self, mode=0, min_char_count=None):
-        # first, check if we've already counted the documents in that collection this session
-        
-        # if so, grab that number. If not, calculate it
+    def save_challenge_results(self, challenge):
+        # get context data
+        school_id = ChallengeManagement._context.get_school_id()
+        user_id = ChallengeManagement._context.get_user_id()
+
+        # TODO: Validate the challenge results in any way? Not sure if necessary as no user input
+
+        response = self._service.save_challenge_results(school_id, user_id, challenge)
+        return response
+
+    def get_random_challenge_content(self, mode=0):
+        """Gets a challenge content text belonging to a particular challenge mode.
+        Will pick a random one.
+        """
+        # first, check if we've already counted the documents in that collection this session        
+        # if so, grab that number. If not, calculate it and save it for next time.
         attribute_name = "mode_" + str(mode) + "_count"
         if hasattr(self._context, attribute_name):
             # max_index here is *ZERO-BASED*
--- services/challenge_service.py
+++ services/challenge_service.py
@@ -1,26 +1,37 @@
-from managements.application_management import ApplicationManagement
-from services.challenge_service import ChallengeService
-from random import randint
+from services.api_service import API
 
-class ChallengeManagement(ApplicationManagement):
+class ChallengeService:
 
-    _service = ChallengeService()
+    api = None
 
-    def get_random_challenge_content(self, mode=0, min_char_count=None):
-        # first, check if we've already counted the documents in that collection this session
-        
-        # if so, grab that number. If not, calculate it
-        attribute_name = "mode_" + str(mode) + "_count"
-        if hasattr(self._context, attribute_name):
-            # max_index here is *ZERO-BASED*
-            max_index = getattr(self._context, attribute_name)
-        else:
-            max_index = self._service.get_mode_count(mode)
-            setattr(self._context, attribute_name, max_index)
+    def __init__(self):
+        if not ChallengeService.api:
+            ChallengeService.api = API.get_api()
 
-        # calculate a random number less/equal to the max_index
-        request_index = randint(0, max_index)
+    def save_challenge_results(self, school_id, user_id, challenge):
+        """ Saves the challenge results belonging to the specific school/user.
+        Does not do any kind of validation on the challenge, that should be completed before getting to this point.
+        """
+        path = "Schools/" + school_id + "/UserProfiles/" + user_id + "/History"
+        response = ChallengeService.api.post(path=path, data=challenge)
+        return response["document"]
 
-        # then, make our call to get the challenge
-        challenge_content = self._service.get_challenge_content(mode, request_index)
-        return challenge_content
+    def get_mode_count(self, mode):
+        """Counts how many documents are in a particular mode collection.
+        All of those documents are expected to have a incrementing index field.
+        Just finds the document with the highest index and returns the index.
+        Index is 0-based, so it's technically the count - 1.
+        """
+        path = "Challenge/Content/Modes/" + str(mode)
+        order_by = [{"index": False}]
+        limit = 1
+        result = ChallengeService.api.get(path=path, order_by=order_by, limit=limit)
+        return result["index"]
+
+    def get_challenge_content(self, mode, request_index):
+        """Returns a specific challenge's text content for typing.
+        """
+        path = "ChallengeContent/Modes/" + str(mode)
+        where_clause = [["index", "==", request_index]]
+        result = ChallengeService.api.get(path=path, where_clauses=where_clause)
+        return result["text_content"]
