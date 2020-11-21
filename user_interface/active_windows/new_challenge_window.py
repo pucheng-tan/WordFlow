c3676bf8eeef106dd550158e8889d01bf6dbf6f0
Cameron Mcleod <cvm791@mail.usask.ca>
2020-11-21 14:31:05 -0600

    You can now choose time on the create a typing test menu, that time will be put in the variable self.challenge_duration

--- user_interface/active_windows/new_challenge_window.py
+++ user_interface/active_windows/new_challenge_window.py
@@ -14,6 +14,9 @@ class NewChallengeWindow(active_window.ActiveWindow):
 
     def choose_a_typing_challenge(self):
         self.label['text'] = "Choose a typing challenge!"
+
+        self.create_timer()
+
         
         #This variable will be set the the type of test the user wants
         self.challenge_type = tk.StringVar(self.frame)
@@ -25,6 +28,48 @@ class NewChallengeWindow(active_window.ActiveWindow):
     
         submitButton = tk.Button(self.frame, text="Start",command=self._loadTypingChallenge)
         submitButton.pack()
+
+    def create_timer(self):
+        """Creates the labels for the timer frame.
+        Creates both the permanent labels that do not change and the labels the
+        labels that do change when a button is pressed.
+        """
+        self.challenge_duration = "02:00"
+
+        #create timer labels (time_label displays time)
+        self.timer_frame = tk.LabelFrame(self.frame, borderwidth=0)
+        self.timer_frame.pack()
+        self.duration_label = tk.Label(self.timer_frame, text="Duration:")
+        self.time_label = tk.Label(self.timer_frame, text=self.challenge_duration, font=("TkDefaultFont", 15), borderwidth=3, relief="sunken")
+
+        self.duration_label.grid(row=1, column=0,rowspan=2)
+        self.time_label.grid(row=1, column=1, rowspan=2)
+        #display buttons
+        self.up_button = tk.Button(self.timer_frame, text="\u25b2", fg="blue", bg="white")
+        self.down_button = tk.Button(self.timer_frame, text="\u25bc", fg="blue", bg="white")
+
+        self.up_button["command"] = self._up_button_response
+        self.down_button["command"] = self._down_button_response
+
+        self.up_button.grid(row=1, column=2)
+        self.down_button.grid(row=2, column=2)
+
+        
+    def _up_button_response(self):
+        """Increases the amount of time."""
+        total_time = int(self.challenge_duration[1]) + 1
+        if total_time <= 5:
+            self.challenge_duration = "0"+str(total_time)+":00"
+            self.time_label.configure(text=self.challenge_duration)
+            self.timer_frame.update()
+    def _down_button_response(self):
+        """Decreases the amount of time."""
+        total_time = int(self.challenge_duration[1]) - 1
+        if total_time >= 1:
+            self.challenge_duration = "0"+str(total_time)+":00"
+            self.time_label.configure(text=self.challenge_duration)
+            self.timer_frame.update()
+
 
     def _loadTypingChallenge(self):
         
