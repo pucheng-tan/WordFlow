beeeb2e0f156687c0106149892da6ef35437791a
taramepp <taramepp@gmail.com>
2020-11-24 15:10:11 -0600

    Made a few minor changes before approving merge request: - removed extra report.json file - renamed test_gui to mock_gui because it's not a file running tests - Moved the timer folder into PoC because it's not actually being used - deleted services/test.py because that is a junk file that I use for sandboxing and it doesn't need to be on master.

--- run_gui.py
+++ run_gui.py
@@ -1,10 +1,10 @@
 import tkinter as tk
 #THIS IS NOW A FUNCTION TO RUN TEST GUI
 #THE PURPOSE OF TEST GUI IS A GUI THAT DOES NOT DEPEND ON THE LOGIN SCREEN -> PERFECT FOR MAKING QUICK ADJUSTMENTS TO THE GUI
-from user_interface import test_gui
+from user_interface import mock_gui
 
 root = tk.Tk()
-test_gui.TESTGUI(root)
+mock_gui.TESTGUI(root)
 # gui.GUI("Super-Admin", root)
 root.mainloop()
 
