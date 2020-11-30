9463197137b4a26fa1f41723da3490b883b14a9f
Sarah Chen (sbc806) <sbc806@mail.usask.ca>
2020-11-29 22:26:35 -0600

    The self also needed to be removed from screen handler.

--- gui_authentication/screen_handler.py
+++ gui_authentication/screen_handler.py
@@ -3,13 +3,13 @@ from gui_authentication import signup
 from gui_authentication import create_school
 
 
-def run_login_screen(self, master):
+def run_login_screen(master):
     login.Authentication(master).mainloop()
 
-def run_signup_screen(self, master):
+def run_signup_screen(master):
     signup.CreateUser(master).mainloop()
 
-def run_create_school(self, master):
+def run_create_school(master):
     create_school.CreateSchool(master).mainloop()
 
 
