68d294acf623848cbec9df4023905d643dfab728
taramepp <taramepp@gmail.com>
2020-12-02 20:35:03 -0600

    A list with next/previous buttons is now a reusable component. It needs a parent frame, column headings and the data it corresponds to, as well as a function passed in that gets more data. It'll handle the rest. I had originally written my_history_window with this in mind, so it was mostly separating one file into two files.

--- user_interface/active_windows/my_history_window.py
+++ user_interface/active_windows/my_history_window.py
@@ -4,24 +4,9 @@ from user_interface.active_windows import active_window
 from managements import challenge_management
 from datetime import datetime
 
+from user_interface.components import list_view
+
 class MyHistoryWindow(active_window.ActiveWindow):
-
-    # the columns and which attributes they're bound to
-    COLUMN_HEADINGS = ["Date", "Mode", "WPM", "Accuracy"]
-    COLUMN_DATA_FIELDS = ["date_text", "mode", "wpm", "accuracy"]
-
-    # the number of challenge results to show on a page
-    PAGE_LIMIT = 10
-
-    ALT_ROW_COLOUR = "lightgrey"
-
-    BUTTON_FG = "white"
-    BUTTON_BG = "blue"
-    BUTTON_FONT = ("Helvetica", 20)
-
-    DEFAULT_FONT = ("Helvetica", 20)
-    HEADER_FONT = ("Helvetica", 18)
-
     TITLE_FONT = ("Helvetica", 25, "bold")    
     TITLE = "My History"    
 
@@ -35,118 +20,21 @@ class MyHistoryWindow(active_window.ActiveWindow):
         active_window.ActiveWindow.__init__(self, gui)
 
         self.challenge_management = challenge_management.ChallengeManagement()
-        self.current_page_data = self.load_challenge_results()
-        self.pages = [self.current_page_data]
-        self.current_page = 0 # first page
+        self.create_heading()    
 
-        self.create_heading()        
-        self.setup_elements()
-        self.generate_table()
+        HEADINGS = ["Date", "Mode", "WPM", "Accuracy"]
+        FIELDS = ["date_text", "mode", "wpm", "accuracy"]
 
-
-        self.update_table_data()
+        self.history_list = list_view.ListView(self.frame, HEADINGS, FIELDS, self.load_challenge_results)
 
     def create_heading(self):
+        """Creates a header row with a title
+        """
         self.heading_frame = tk.Frame(self.frame)
         self.heading_frame.pack(side=tk.TOP, fill=tk.X)
         title_label = tk.Label(self.heading_frame, text=MyHistoryWindow.TITLE)
         title_label["font"] = MyHistoryWindow.TITLE_FONT
         title_label.pack(side=tk.LEFT, padx=MyHistoryWindow.PADX, pady=MyHistoryWindow.PADY)
-        
-    def setup_elements(self):
-        self.next_button = tk.Button(self.heading_frame, 
-                                text="Next", 
-                                fg=MyHistoryWindow.BUTTON_FG, 
-                                bg=MyHistoryWindow.BUTTON_BG, 
-                                font=MyHistoryWindow.BUTTON_FONT)
-        self.prev_button = tk.Button(self.heading_frame, 
-                                text="Previous", 
-                                fg=MyHistoryWindow.BUTTON_FG, 
-                                bg=MyHistoryWindow.BUTTON_BG, 
-                                font=MyHistoryWindow.BUTTON_FONT)
-
-        self.prev_button["state"] = tk.DISABLED
-        self.next_button["command"] = self.view_next_page
-        self.prev_button["command"] = self.view_previous_page
-        
-        self.next_button.pack(side=tk.RIGHT, padx=MyHistoryWindow.PADX, pady=MyHistoryWindow.PADY)
-        self.prev_button.pack(side=tk.RIGHT, padx=MyHistoryWindow.PADX, pady=MyHistoryWindow.PADY)
-
-    def generate_table(self):
-        # parent frame for table
-        self.table_frame = tk.Frame(self.frame)
-        self.table_frame.pack(side=tk.TOP, fill=tk.X, expand=True, padx=MyHistoryWindow.PADX)
-
-        # scroll bar in the table
-        self.scroll_bar = tk.Scrollbar(self.table_frame)
-        self.scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
-
-        # table itself, and more scrollbar stuff
-        self.data_table = ttk.Treeview(self.table_frame, 
-                                       yscrollcommand=self.scroll_bar.set, 
-                                       selectmode="browse", 
-                                       style="Custom.Treeview",
-                                       show=["headings"])
-        self.scroll_bar.config(command=self.data_table.yview)
-        self.data_table.pack(fill=tk.X)
-
-        self.data_table["columns"] = MyHistoryWindow.COLUMN_HEADINGS
-        for heading in MyHistoryWindow.COLUMN_HEADINGS:
-            self.data_table.heading(heading, text=heading, anchor=tk.CENTER)
-
-        table_style = ttk.Style()
-        table_style.configure("Treeview.Heading", font=MyHistoryWindow.HEADER_FONT)
-        table_style.configure("Treeview", font=MyHistoryWindow.DEFAULT_FONT, rowheight=40)
-        self.data_table.tag_configure("odd", background=MyHistoryWindow.ALT_ROW_COLOUR)
-
-
-    def update_table_data(self):
-        """Changes the data in the table
-        """
-        # nuke all the current data
-        self.data_table.delete(*self.data_table.get_children())
-
-        # then put in data for all of the current page
-        print("Current page number: " + str(self.current_page))
-        self.current_page_data = self.pages[self.current_page]
-        
-        for i in range(0, len(self.current_page_data)):
-            data = self.current_page_data[i]
-            values = ([data[field] for field in MyHistoryWindow.COLUMN_DATA_FIELDS])
-
-            tag = "even" if i % 2 == 0 else "odd"
-            self.data_table.insert(parent="", index="end", iid=i, values=values, tags=(tag))
-
-
-    def view_previous_page(self):
-        # updates the data to the previous page data, don't pull again
-        self.current_page = self.current_page - 1
-        # disable the button if applicable, enable next button
-        if self.current_page == 0:
-            self.prev_button["state"] = tk.DISABLED
-        self.next_button["state"] = tk.NORMAL
-        self.update_table_data()
-
-    def view_next_page(self):
-        # update page number
-        self.current_page = self.current_page + 1
-
-        # if we don't have the page data, get it
-        if self.current_page > len(self.pages) - 1:
-            print("Time to load a new page!")
-            
-            # do the stuff to get the data
-            last_challenge = None
-            if self.current_page_data: # FALSE
-                last_challenge = self.current_page_data[-1]
-
-            new_data = self.load_challenge_results(last_challenge)
-            self.pages.append(new_data)
-        
-        # enable the previous button if applicable
-        if self.current_page == 1:
-            self.prev_button["state"] = tk.NORMAL
-        self.update_table_data()
         
     def format_data(self, data):
         # turn the data into a list
@@ -163,36 +51,14 @@ class MyHistoryWindow(active_window.ActiveWindow):
             item["date_text"] = date_text
         return data_list
 
-    def load_challenge_results(self, last_challenge=None):
+    def load_challenge_results(self, limit, last_challenge=None):
         """ Loads the challenge results data based on the last challenge that is currently in the list.
         Puts that data in self.challenge_results
         Args:
             last_challenge: on a "page" of challenges, the final challenge on that list
         """
         # Define the date to start at as either now if the list is empty, or the last challenge otherwise
-        print("This is the last challenge:")
-        challenge = "None" if not last_challenge else last_challenge["date_text"]
-        print(challenge)
         start_at = datetime.now() if not last_challenge else last_challenge["date_completed"]
         # get the data
-        results = self.challenge_management.get_my_challenge_history(start_time=start_at, limit=MyHistoryWindow.PAGE_LIMIT)
-        
-        
-        # check if it's good
-        if "error" not in results:
-            # disable the "next" button if necessary
-            self.check_if_out_of_data(results)
-            # change the mode to something readable
-            return self.format_data(results)
-        else:
-            self.display_error(results["error"])
-            return []
-
-    def check_if_out_of_data(self, results):
-        if len(results) < self.PAGE_LIMIT:
-            # disable the next button
-                self.next_button["state"] = tk.DISABLED
-
-    def display_error(self, error_message):
-        # TODO: this should be actually displayed on the page
-        print(error_message)
+        challenges = self.challenge_management.get_my_challenge_history(start_time=start_at, limit=limit)
+        return self.format_data(challenges)
---
+++ user_interface/components/list_view.py
@@ -0,0 +1,187 @@
+import tkinter as tk
+from tkinter import ttk
+
+class ListView():
+
+    # the number of items to show on a page
+    PAGE_LIMIT = 10
+
+    ALT_ROW_COLOUR = "lightgrey"
+
+    BUTTON_FG = "white"
+    BUTTON_BG = "blue"
+    BUTTON_FONT = ("Helvetica", 20)
+
+    DEFAULT_FONT = ("Helvetica", 20)
+    HEADER_FONT = ("Helvetica", 18)
+
+    PADX = 20
+    PADY = 20
+
+
+    def __init__(self, parent_frame, column_headings, column_data_fields, load_data_function):
+        """Creates a table with headings that paginates (has next/prev buttons)
+
+        Args:
+            parent_frame: the frame in which to place the table
+            column_headings: ordered list of headings to be displayed
+            column_data_fields: list of data attributes that match up to those headings
+            load_data_function: The function used to call the data
+                It's expected to take a limit (int) parameter, and an item parameter,
+                that it will use to find the next item on the page (see my_history_window)
+                It should take care of formatting the data (ie: dates, any mapping),
+                and should return a list of data items
+        """
+        # initialize variables to parameters
+        self.column_headings = column_headings
+        self.column_data_fields = column_data_fields
+        self.load_data_function = load_data_function
+
+        # start setting up page data
+        self.current_page_data = self.load_data_function(ListView.PAGE_LIMIT)
+        self.pages = [self.current_page_data]
+        self.current_page = 0 # first page
+
+        # make the gui things      
+        self.generate_buttons(parent_frame)
+        self.generate_table(parent_frame)
+        self.update_table_data()
+        
+    def generate_buttons(self, parent_frame):
+        """ Create the next and previous buttons
+        """
+        self.buttons_frame = tk.Frame(parent_frame)
+        self.buttons_frame.pack(side=tk.BOTTOM, fill=tk.X)
+        self.next_button = tk.Button(self.buttons_frame, 
+                                text="Next", 
+                                fg=ListView.BUTTON_FG, 
+                                bg=ListView.BUTTON_BG, 
+                                font=ListView.BUTTON_FONT)
+        self.prev_button = tk.Button(self.buttons_frame, 
+                                text="Previous", 
+                                fg=ListView.BUTTON_FG, 
+                                bg=ListView.BUTTON_BG, 
+                                font=ListView.BUTTON_FONT)
+
+        self.prev_button["state"] = tk.DISABLED
+        self.next_button["command"] = self.view_next_page
+        self.prev_button["command"] = self.view_previous_page
+        
+        self.next_button.pack(side=tk.RIGHT, padx=ListView.PADX, pady=ListView.PADY)
+        self.prev_button.pack(side=tk.RIGHT, padx=ListView.PADX, pady=ListView.PADY)
+
+    def generate_table(self, parent_frame):
+        """Create the table part of it
+        """
+        # parent frame for table
+        self.table_frame = tk.Frame(parent_frame)
+        self.table_frame.pack(side=tk.TOP, fill=tk.X, expand=True, padx=ListView.PADX)
+
+        # scroll bar in the table
+        self.scroll_bar = tk.Scrollbar(self.table_frame)
+        self.scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        # table itself, and more scrollbar stuff
+        self.data_table = ttk.Treeview(self.table_frame, 
+                                       yscrollcommand=self.scroll_bar.set, 
+                                       selectmode="browse", 
+                                       style="Custom.Treeview",
+                                       show=["headings"])
+        self.scroll_bar.config(command=self.data_table.yview)
+        self.data_table.pack(fill=tk.X)
+
+        # make the header row
+        self.data_table["columns"] = self.column_headings
+        for heading in self.column_headings:
+            self.data_table.heading(heading, text=heading, anchor=tk.CENTER)
+
+        # and then style the table
+        table_style = ttk.Style()
+        table_style.configure("Treeview.Heading", font=ListView.HEADER_FONT)
+        table_style.configure("Treeview", font=ListView.DEFAULT_FONT, rowheight=40)
+        self.data_table.tag_configure("odd", background=ListView.ALT_ROW_COLOUR)
+
+
+    def update_table_data(self):
+        """Changes the data in the table
+        """
+        # nuke all the current data
+        self.data_table.delete(*self.data_table.get_children())
+
+        # then put in data for all of the current page
+        self.current_page_data = self.pages[self.current_page]
+        
+        for i in range(0, len(self.current_page_data)):
+            data = self.current_page_data[i]
+            values = ([data[field] for field in self.column_data_fields])
+
+            tag = "even" if i % 2 == 0 else "odd"
+            self.data_table.insert(parent="", index="end", iid=i, values=values, tags=(tag))
+
+
+    def view_previous_page(self):
+        """Called by clicking the previous button, it'll go to the previous page
+        Will enable the "next" button, and disable itself if at first page
+        """
+        # updates the data to the previous page data, don't pull again
+        self.current_page = self.current_page - 1
+        # disable the button if applicable, enable next button
+        if self.current_page == 0:
+            self.prev_button["state"] = tk.DISABLED
+        self.next_button["state"] = tk.NORMAL
+        self.update_table_data()
+
+    def view_next_page(self):
+        """Called by clicking the next button,
+        it'll make the calls to update the data, 
+        possibly enable the previous button, and then
+        call to update the table data
+        """
+        # update page number
+        self.current_page = self.current_page + 1
+
+        # if we don't have the page data, get it
+        if self.current_page > len(self.pages) - 1:
+            
+            # do the stuff to get the data
+            last_item = None
+            if self.current_page_data:
+                last_item = self.current_page_data[-1]
+
+            new_data = self.load_new_data(last_item)
+            self.pages.append(new_data)
+        
+        # enable the previous button if applicable
+        if self.current_page == 1:
+            self.prev_button["state"] = tk.NORMAL
+        self.update_table_data()
+        
+    def load_new_data(self, last_item=None):
+        """Calls the passed in load data function to get more data.
+        Checks it for errors, and that it exists.
+        
+        Args:
+            last_item: the last item on the current page to be compared for the next page
+        """
+        # get the data
+        data_items = self.load_data_function(ListView.PAGE_LIMIT, last_item)
+        # and then check that it's all good
+        if "error" not in data_items:
+            self.check_if_out_of_data(data_items)
+            return data_items
+        else:
+            self.display_error(data_items["error"])
+            return []
+
+    def check_if_out_of_data(self, results):
+        """Checks if we're out of data and if the next button should be disabled.
+        If the most recent page is the final page, but it's exactly the page limit,
+        it won't be caught until the next button is clicked again
+        """
+        if len(results) < self.PAGE_LIMIT:
+            # disable the next button
+                self.next_button["state"] = tk.DISABLED
+
+    def display_error(self, error_message):
+        # TODO: this should be actually displayed on the page
+        print(error_message)
