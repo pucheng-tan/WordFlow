5487fc8ec61dd89434843166b73654f5eb57e5fe
DESKTOP-QAQ5QQV\Sarah <sarahc19233@gmail.com>
2020-12-06 00:33:16 -0600

    Made help information for standard, admin, and super-admin users. Pair Programming with Pucheng.

--- user_interface/active_windows/help_window.py
+++ user_interface/active_windows/help_window.py
@@ -7,24 +7,92 @@ class HelpWindow(active_window.ActiveWindow):
 
         self.display_help_information()
 
+        if self.privilege < 2:
+            self.add_admin_help_information()
+        if self.privilege == 0:
+            self.add_super_admin_help_information()
+
+        self.help_text.configure(state="disabled")
 
     def display_help_information(self):
         help_scroll_bar = tk.Scrollbar(self.frame)
-        help_text = tk.Text(self.frame)
+        self.help_text = tk.Text(self.frame)
         help_scroll_bar.pack(side=tk.RIGHT, fill=tk.Y)
-        help_text.pack(side=tk.LEFT, fill=tk.BOTH)
-        help_scroll_bar.config(command=help_text.yview)
-        help_text.config(yscrollcommand=help_scroll_bar.set)
-        quote = """HAMLET: To be, or not to be--that is the question:
-        Whether 'tis nobler in the mind to suffer
-        The slings and arrows of outrageous fortune
-        Or to take arms against a sea of troubles
-        And by opposing end them. To die, to sleep--
-        No more--and by a sleep to say we end
-        The heartache, and the thousand natural shocks
-        That flesh is heir to. 'Tis a consummation
-        Devoutly to be wished."""
-        help_text.insert(tk.END, quote)
+        self.help_text.pack(side=tk.LEFT, fill=tk.BOTH)
+        help_scroll_bar.config(command=self.help_text.yview)
+        self.help_text.config(yscrollcommand=help_scroll_bar.set)
+        help_information = """
+        Welcome to Word Flow!
+        This is a typing tool that helps users improve their typing speed. The
+        target users are any computer users that wish to improve their speed.
+        We want to make tests that fit different types of users, including
+        programmers, professional typists, TBD. The user is given text to type,
+        and metrics are recorded and displayed at the end of the test. We hope
+        to develop different types of metrics that can help users track their
+        improvement!
+        
+        Taking a typing challenge:
+        1. Click New Challenge in the main menu.
+        2. Select a mode and a time.
+        3. Click Start.
+        
+        Changing your profile information:
+        1. Click My Profile in the main menu.
+        2. Select a feature to change and click the button.
+        3. Enter in the new information.
+        4. Click My Profile again to see the change.
+        """
+        self.help_text.insert(tk.END, help_information)
+        self.help_text.configure(font=("Helvetica", 15))
+
+        email_label = tk.Label(self.frame, text="If you have any issues, email us at group2@gmail.com!")
+        email_label["font"] = ("Helvetica", 15)
+        email_label.pack(anchor=tk.S)
 
     # def show(self):
     #     self.frame.pack(fill=tk.BOTH)
+
+    def add_admin_help_information(self):
+        """
+        Adds help information for admin users to text box or the help window.
+        """
+        help_information = """
+        Creating a Classroom
+        1. Click Classroom Management in the main menu. 
+        2. Click the Add button.
+        3. Fill the information required for creating a new Classroom.
+        4. Click the Submit button.
+        """
+        self.help_text.insert(tk.END, help_information)
+
+    def add_super_admin_help_information(self):
+        """
+        Adds help information for super admin users to text box for the help window.
+        """
+        help_information = """
+        Types of Users:
+        There are three of types of users.
+        Standard users are equivalent to students. They can take challenges.
+        Admin users are equivalent to teachers. They can do everything standard
+        users can do and manage classrooms.
+        Super-admin users are equivalent to principals or IT staff. They can do
+        everything that admin users can do and manage users in the school.
+        
+        Adding New Users to the School:
+        1. Click User Management in the main menu.
+        2. Click New User.
+        3. Enter their email address.
+        4. Pick a privilege level.
+        5. Click enter. A verification email will be sent to them.
+        6. Once they have verified their email, they can sign up and then log
+        in.
+        
+        Changing Privilege Level of Users:
+        1. Click User Management in the main menu.
+        2. Select a user in the table.
+        3. Pick a privilege level in the drop down menu.
+        4. Click the button "Change Privilege".
+        Note: The privilege level of super-admin users cannot be changed.
+        """
+
+        self.help_text.insert(tk.END, help_information)
