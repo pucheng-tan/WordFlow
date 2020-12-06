5289979502153ca2b42eabfb3c10ddbf02ecea19
DESKTOP-QAQ5QQV\Sarah <sarahc19233@gmail.com>
2020-12-06 05:05:55 -0600

    Wrote some tests for user_management and user_service. Ran all the tests again in managements and services and they all passed.

--- managements/test_user_management.py
+++ managements/test_user_management.py
@@ -32,9 +32,21 @@ def test_login():
 @pytest.mark.parametrize("email, password, expected_result", [
                         ("1128@gmail.com", "123456", "Email is not verified."),
                         ("bad_email", "123456", "Malformed email"),
-                        ("not_in_firebase@fdjsfk.com", "123456", "No user record found")
+                        ("not_in_firebase@fdjsfk.com", "123456", "No user record found"),
+                        ("", "123456", "Email must be a non-empty string.")
 ])
 def test_signup(email, password, expected_result):
     result = user_management.signup(email, password)
     assert expected_result in result
+
+@pytest.mark.parametrize("user, expected_result", [
+                        ({"email": "bad_email"}, "Malformed email"),
+                        ({"email": "not in firebased@fdjsk.com"}, "No user record found"),
+                        ({"email": ""}, "Email must be a non-empty string.")
+])
+def test_send_invite_email(user, expected_result):
+    result = user_management.send_invite_email(user)
+    assert expected_result in result
+
+
 
